b5995e335034b5e1ee65a30064bbc724eb05c018
Jhay1413 <93253483+Jhay1413@users.noreply.github.com>
2022-01-07 04:47:17 +0800

    final exam

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+
+  public num1!:number;
+  public num2!:number;
+  public result!:number;
+
+  ngOnInit() {
+   this.result = 0;
+  }
+
+  add(){
+    this.result = this.num1+this.num2;
+  }
+  mult(){
+    this.result = this.num1*this.num2;
+  }
+  divide(){
+    this.result = this.num1/this.num2;
+  }
+  subt(){
+    this.result = this.num1-this.num2;
+  }
 }
